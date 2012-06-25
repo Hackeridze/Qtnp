71eb82ae316743efd200e2690697a48e4132bd8f
Victor Hackeridze <hackeridze@gmail.com>
2012-06-25 16:59:18 +0600

    Added license comment to all Qtnp source files.

--- fparser/fparser.cpp
+++ fparser/fparser.cpp
@@ -1,3 +1,20 @@
+/*
+ *This file is part of Qtnp.
+ *
+ * Qtnp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Qtnp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qtnp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "fparser.h"
 #include <ostream>
 
@@ -5,23 +22,20 @@ using namespace mu;
 
 QtnpParser::QtnpParser()
 {
-    Parser p;
+	Parser p;
 };
 
 void QtnpParser::setE(std::string str)
 {
-    p.SetExpr(str);
+	p.SetExpr(str);
 }
 
 double QtnpParser::getR(value_type x)
 {
-    try
-    {
-        p.DefineVar("x",&x);
-    }
-    catch (Parser::exception_type &e)
-    {
-        std::cerr << e.GetMsg() << endl;
-    }
-    return p.Eval();
+	try {
+		p.DefineVar("x",&x);
+	} catch (Parser::exception_type &e) {
+		std::cerr << e.GetMsg() << endl;
+	}
+	return p.Eval();
 }
--- fparser/fparser.h
+++ fparser/fparser.h
@@ -1,20 +1,36 @@
+/*
+ *This file is part of Qtnp.
+ *
+ * Qtnp is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Qtnp is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Qtnp.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef FPARSER_H
 #define FPARSER_H
 
 #include "muparser/muParser.h"
 #include <string>
 
-using namespace std;
 using namespace mu;
 
 class QtnpParser
 {
 private:
-    Parser p;
+	Parser p;
 public:
-    QtnpParser();
-    void setE(string str);
-    double getR(value_type x);
+	QtnpParser();
+	void setE(std::string str);
+	double getR(value_type x);
 };
 
 #endif // FPARSER_H
